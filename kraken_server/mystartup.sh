1600d4ac5bbf6a7578b249ce3f4ca23bb91a7ebd
rurouny79 <90335117+rurouny79@users.noreply.github.com>
2024-12-24 22:46:51 +0900

    Update mystartup.sh

--- kraken_server/mystartup.sh
+++ kraken_server/mystartup.sh
@@ -2,47 +2,33 @@
 
 # hw setting
 ## wol setting
-ethtool --change enp6s0 wol g
-
-## water pump
-liquidctl set pump speed 100
-
-## start netplan (wifi connect)
-while true
-do
-        ip=$(ifconfig wlp6s0 | grep 192.168)
-        if [ $? = 0 ]
-        then
-                break
-        else
-                netplan apply
-                sleep 60
-        fi
-done
+ethtool --change enp7s0 wol g
 
 # start containers
 while true
 do
-        sleep 1
-        if curl -s --unix-socket /var/run/docker.sock http/_ping 2>&1 >/dev/null
-        then
-                break
-        fi
+	sleep 1
+	if curl -s --unix-socket /var/run/docker.sock http/_ping 2>&1 >/dev/null
+	then
+		break
+	fi
 done
 for containerId in $(docker ps -a -q)
 do
-        docker restart $containerId
-        docker exec $containerId service ssh start
-        docker exec $containerId service xrdp start
-        docker exec $containerId service cron start
-        docker exec -d $containerId code-server
-        docker exec -d $containerId code-server_cert.sh
+	docker restart $containerId
+	docker exec $containerId service ssh start
+	docker exec $containerId service xrdp start
+	docker exec $containerId service cron start
+	docker exec -d $containerId code-server
+	docker exec -d $containerId code-server_cert.sh
 done
 docker exec smb service smbd start
 docker exec svn service apache2 start
 
-# start virtual machine instances
-# virsh start xxx
+# start windows virtual machine
+/myopt/portmap_streaming.sh
+virsh start win11
+virsh start ubuntu22.04
 
 # start try sleep (final opt action 1)
 timecnt=0
@@ -71,7 +57,7 @@ do
         #fi
 
 	ldavg=$(cat /proc/loadavg | awk '{print $1;}')
-	ldavg_ret=$(echo "$ldavg > 0.1" | bc)
+	ldavg_ret=$(echo "$ldavg > 2.0" | bc)
         if [ $ldavg_ret -eq "1" ]
         then
                 date >> /tmp/whynotsleep.log
@@ -124,22 +110,5 @@ do
                 echo 0 > /sys/class/rtc/rtc0/wakealarm
                 echo $time_t > /sys/class/rtc/rtc0/wakealarm
 		systemctl suspend
-        fi
-done
-
-# start mining (final opt action 2)
-/myopt/raptoreum/cpuminer-gr-avx2-1.2.4.1/bin/cpuminer.sh &
-sleep 10
-PID=$(pidof "cpuminer-zen3")
-while true
-do
-        sleep 1
-        busycnt=$(ps -eo %cpu,args | awk '$1 >= 3 {print}' | grep -v cpuminer-zen3 | wc -l)
-        if test $busycnt -eq 0
-        then
-                kill -CONT $PID
-        elif test $busycnt -gt 0
-        then
-                kill -STOP $PID
         fi
 done
