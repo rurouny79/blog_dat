03aaca3392d515c1dd336e97282a81537b662ad3
rurouny79 <90335117+rurouny79@users.noreply.github.com>
2024-12-24 23:58:19 +0900

    Update port_monitoring.sh

--- kraken_server/port_monitoring.sh
+++ kraken_server/port_monitoring.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-tcpdump "tcp[tcpflags] & (tcp-syn) != 0" -l -n -i enp7s0 and inbound and dst port '(445 or 443 or 139 or 80 or 22 or 3389)' or "tcp[tcpflags] & (tcp-syn) != 0" -l -n -i enp7s0 and inbound and dst portrange '(1001-1004 or 2001-2003 or 3001-3003 or 3389)'| while read line; do
+tcpdump "tcp[tcpflags] & (tcp-syn) != 0" -l -n -i enp7s0 and inbound and dst port '(445 or 443 or 139 or 80 or 22 or 3389 or 1001 or 1002 or 1003 or 1004 or 2001 or 2002 or 2003 or 3001 or 3002 or 3003)'| while read line; do
 {
 echo To: criny.master@gmail.com
 echo Subject: kraken port warning
