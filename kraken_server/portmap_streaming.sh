c2eaa398c923ba8c396827e947331c9b44299f70
rurouny79 <90335117+rurouny79@users.noreply.github.com>
2024-12-28 12:45:35 +0900

    Update portmap_streaming.sh

--- kraken_server/portmap_streaming.sh
+++ kraken_server/portmap_streaming.sh
@@ -1,19 +1,19 @@
-# parameter
-TARGET=81
+# ubuntu22.04_gpu: enable rdp
+TARGET=194
+iptables -I INPUT -p tcp -s 0.0.0.0/0 -d 192.168.0.2 --dport 53389 -j ACCEPT
+iptables -I FORWARD -m tcp -p tcp -d 192.168.122.$TARGET --dport 3389 -j ACCEPT
+iptables -I FORWARD -m state -p tcp -d 192.168.0.0/24 --state NEW,RELATED,ESTABLISHED -j ACCEPT
+iptables -t nat -I PREROUTING -p tcp -d 192.168.0.2 --dport 53389 -j DNAT --to-destination 192.168.122.$TARGET:3389
 
 # win11: enable rdp
+TARGET=81
 iptables -I INPUT -p tcp -s 0.0.0.0/0 -d 192.168.0.2 --dport 43389 -j ACCEPT
 iptables -I FORWARD -m tcp -p tcp -d 192.168.122.$TARGET --dport 3389 -j ACCEPT
 iptables -I FORWARD -m state -p tcp -d 192.168.0.0/24 --state NEW,RELATED,ESTABLISHED -j ACCEPT
 iptables -t nat -I PREROUTING -p tcp -d 192.168.0.2 --dport 43389 -j DNAT --to-destination 192.168.122.$TARGET:3389
 
-# ubuntu22.04: enable rdp
-iptables -I INPUT -p tcp -s 0.0.0.0/0 -d 192.168.0.2 --dport 53389 -j ACCEPT
-iptables -I FORWARD -m tcp -p tcp -d 192.168.122.223 --dport 3389 -j ACCEPT
-iptables -I FORWARD -m state -p tcp -d 192.168.0.0/24 --state NEW,RELATED,ESTABLISHED -j ACCEPT
-iptables -t nat -I PREROUTING -p tcp -d 192.168.0.2 --dport 53389 -j DNAT --to-destination 192.168.122.223:3389
-
 # win11: enable sunshine
+TARGET=81
 iptables -I INPUT -p tcp -s 0.0.0.0/0 -d 192.168.0.2 --dport 47984 -j ACCEPT
 iptables -I INPUT -p udp -s 0.0.0.0/0 -d 192.168.0.2 --dport 47989 -j ACCEPT
 iptables -I INPUT -p udp -s 0.0.0.0/0 -d 192.168.0.2 --dport 47998 -j ACCEPT
@@ -22,7 +22,6 @@ iptables -I INPUT -p udp -s 0.0.0.0/0 -d 192.168.0.2 --dport 48000 -j ACCEPT
 iptables -I INPUT -p udp -s 0.0.0.0/0 -d 192.168.0.2 --dport 48002 -j ACCEPT
 iptables -I INPUT -p tcp -s 0.0.0.0/0 -d 192.168.0.2 --dport 48010 -j ACCEPT
 iptables -I INPUT -p udp -s 0.0.0.0/0 -d 192.168.0.2 --dport 48010 -j ACCEPT
-
 
 iptables -I FORWARD -m tcp -p tcp -d 192.168.122.$TARGET --dport 47984 -j ACCEPT
 iptables -I FORWARD -m tcp -p tcp -d 192.168.122.$TARGET --dport 47989 -j ACCEPT
@@ -33,10 +32,8 @@ iptables -I FORWARD -m udp -p udp -d 192.168.122.$TARGET --dport 48002 -j ACCEPT
 iptables -I FORWARD -m tcp -p tcp -d 192.168.122.$TARGET --dport 48010 -j ACCEPT
 iptables -I FORWARD -m udp -p udp -d 192.168.122.$TARGET --dport 48010 -j ACCEPT
 
-
 iptables -I FORWARD -m state -p tcp -d 192.168.0.0/24 --state NEW,RELATED,ESTABLISHED -j ACCEPT
 iptables -I FORWARD -m state -p udp -d 192.168.0.0/24 --state NEW,RELATED,ESTABLISHED -j ACCEPT
-
 
 iptables -t nat -I PREROUTING -p tcp -d 192.168.0.2 --dport 47984 -j DNAT --to-destination 192.168.122.$TARGET:47984
 iptables -t nat -I PREROUTING -p tcp -d 192.168.0.2 --dport 47989 -j DNAT --to-destination 192.168.122.$TARGET:47989
